aedc28c9048a478f8df4ac02dc3f3eaeb4de32b4
github username <github email address>
2018-08-03 10:43:02 +0100

    crushed it

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,5 +11,25 @@ class Movie
   def self.all
     @@all
   end
+def queue_items
+arr =QueueItem.all.select{|queue_item| queue_item.movie == self}
+# arr.map{|queue_item| queue_item.}
 
+end
+
+def viewers
+arr =QueueItem.all.select{|queue_item| queue_item.movie == self}
+arr.map {|queue_item| queue_item.viewer}
+end
+
+def average_rating
+# arr =QueueItem.all.select{|queue_item| queue_item.movie == self}
+
+arr = queue_items.map{|queue_item| queue_item.rating}
+arr.inject(0){|sum,e| sum+e}/arr.length
+end
+ def highest_rated
+   arr = queue_items.map{|queue_item| queue_item.rating}
+   arr.sort.last
+ end
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,33 @@ class Viewer
   def self.all
     @@all
   end
-  
+  def queue_items
+QueueItem.all.select{|queue_item| queue_item.viewer == self}
+
+  end
+
+  def queue_movies
+
+queue_items.map {|queue_item| queue_item.movie}
+
+  end
+
+  def add_movie_to_queue(movie)
+QueueItem.new(movie, self)
+
+  end
+
+def rate_movie(movie, rating)
+# arr = QueueItem.all.select{|queue_item| queue_item.viewer == self}
+#  arr_1 =QueueItem.all.select {|queue_item| queue_item.movie==movie }
+#  i
+arr =QueueItem.all.select{|queue_item| queue_item.viewer == self}
+arr_1 =QueueItem.all.select {|queue_item| queue_item.movie==movie }
+# if arr_1.include?(movie)
+# QueueItem.new(movie, self, rating)
+# else
+#   nil
+# end
+end
+
 end
